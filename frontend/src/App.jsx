8c78d86b1eb6f4cdcaaad0b51fa66c039e52f08b
szittja <szittja21@gmail.com>
2026-06-02 07:48:55 +0200

    Átállás fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -58,8 +58,8 @@ function App() {
 
   useEffect(() => {
     const checkTimeWindow = () => {
-      //const now = new Date() // Élesben ez kell!
-      const now = new Date('2026-05-26T13:00:00Z') 
+      const now = new Date() // Élesben ez kell!
+      //const now = new Date('2026-05-26T13:00:00Z') 
       const day = now.getDay()
       const hours = now.getHours()
       
